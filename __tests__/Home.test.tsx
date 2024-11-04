57ab04610d21947b74e4044f32c35c8596b4c816
Yigit-Yeler <yeleryigit40@gmail.com>
2024-11-04 11:10:28 +0300

    Updated Test

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,18 +1,12 @@
-import Home from '@/pages'
-import '@testing-library/jest-dom'
-import { render, screen, waitFor } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import Home from '@/pages';
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 
 it('should display city weather data based on selected city', async () => {
-    const darkMode = false;
-    const setDarkMode = jest.fn();
-
-
-    const city = { label: 'Eskişehir', id: 26 }
-
     render(
-        <Home darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Home />
     );
     const cityInputElement = await screen.findByTestId("city-input");
     await userEvent.type(cityInputElement, "Eskişehir")
@@ -25,27 +19,3 @@ it('should display city weather data based on selected city', async () => {
     });
 
 });
-
-
-
-// it('should have Docs text', () => {
-//     render(<Example />)
-
-//     const myElement = screen.getByText("Example Component")
-//     // console.log(myElement)
-
-//     expect(myElement).toBeInTheDocument()
-// })
-
-
-// it('should city name display correct', async () => {
-//     render(<CurrentWeatherInfoCard city={{ label: "Eskişehir", id: 26 }} />)
-
-//     const myElement = await screen.findByTestId("city")
-//     console.log(myElement)
-
-
-//     await waitFor(() => {
-//         expect(myElement).toHaveTextContent("Eskişehir")
-//     })
-// })
